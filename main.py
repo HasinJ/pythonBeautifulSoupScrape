ee3857af357eac0d69c03861383db6986a653dd3
HasinJ <39993584+HasinJ@users.noreply.github.com>
2020-03-17 20:17:10 -0400

    webscrape for allsides: 1st and 2nd cell

--- main.py
+++ main.py
@@ -2,6 +2,35 @@ def save_html(html, path): #local save
     with open(path,'wb') as f: #wb 'write bytes', avoid encoding issues #same as file opening and closing + exception handling
         f.write(html)
 
-def open_html(path) #open/read HTML from local file
+#save_html(r.content, google_com)
+
+def open_html(path): #open/read HTML from local file
     with open(path,'rb') as f:
         return f.read
+
+#open_html(google_com)
+
+import requests
+from bs4 import BeautifulSoup #module
+
+#saving, so that there is a need for only one request
+url = 'https://www.allsides.com/media-bias/media-bias-ratings'
+r = requests.get(url)
+
+soup = BeautifulSoup(r.content, 'html.parser') #soup becomes an object with MANY methods
+
+#News Source
+rows = soup.select('tbody tr')
+row = rows[0]
+name = row.select_one('.source-title').text.strip()
+print(name)
+
+#reference page for News Source
+allsides_page = row.select_one('.source-title a')['href']
+allsides_page = 'https://www.allsides.com' + allsides_page
+print('reference: ' + allsides_page)
+
+#bias
+bias = row.select_one('.views-field-field-bias-image a')['href']
+bias = bias.split('/')[-1] #string operation, like javascript
+print(bias)
