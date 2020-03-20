0f34dc1d7e8ef883dc032bda7b3125ea88b3e006
HasinJ <39993584+HasinJ@users.noreply.github.com>
2020-03-20 01:46:46 -0400

    column names

--- main.py
+++ main.py
@@ -13,5 +13,13 @@ f = open('Report.xls','rb')
 content = f.read()
 soup = BeautifulSoup(content,'html.parser')
 rows = soup.select_one("table tr")
-print(rows.select_one('td').text.strip())
+
+print(rows)
+elements = rows.select('td')
+
+for x in range(len(elements)):
+    print(rows.select('td')[x].text.strip())
+
+print(rows.select('td')[0].text.strip())
+
 f.close()
